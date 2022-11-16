3c145b2b011c83e42209ac55e2e45aede06e2536
Emad Ali <mc.lilzee@hotmail.com>
2022-11-16 12:50:56 +0100

    Create recipes class

---
+++ src/main/java/project/recipes/Recipe.java
@@ -0,0 +1,15 @@
+package project.recipes;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
+public class Recipe {
+
+    private String name;
+    private String description;
+    private String ingredients;
+    private String directions;
+}

97adb75665cf46ca4f5f96bcb4f5f4ec80ef401a
Emad Ali <mc.lilzee@hotmail.com>
2022-11-17 22:08:18 +0100

    Add user

---
+++ src/main/java/project/recipes/business/user/User.java
@@ -0,0 +1,26 @@
+package project.recipes.business.user;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Setter @Getter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Email
+    private String email;
+
+    @Size(min = 8)
+    private String password;
+}
